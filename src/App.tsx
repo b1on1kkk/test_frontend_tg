94c0f3150dacceeab0c92474fd02372d490a1c93
b1on1kkk <b1on1kkk@mail.ru>
2024-06-16 01:42:15 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
-import { useMemo } from "react";
-import "./App.css";
+import { useEffect, useMemo, useState } from "react";
+
+import axios from "axios";
+
+async function getUser(user: WebAppUser) {
+  const { id, username, photo_url } = user;
+
+  try {
+    const data = await axios.post(
+      "http://localhost:3000/users/find_or_create_user",
+      {
+        _id: id,
+        image: photo_url,
+        name: username
+      }
+    );
+
+    return data.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 function App() {
+  const [user, setUser] = useState<any>(null);
+
   const tg = useMemo(() => {
     return window.Telegram.WebApp;
   }, []);
 
-  console.log(tg);
-
-  const user_data = useMemo(() => {
-    if (tg) return tg.initDataUnsafe;
+  useEffect(() => {
+    if (tg && tg.initDataUnsafe.user) setUser(getUser(tg.initDataUnsafe.user));
   }, [tg]);
 
-  return (
-    <div>
-      {user_data ? <>{JSON.stringify(user_data.user)}</> : <>{undefined}</>}
-    </div>
-  );
+  return <div>{user ? <>{JSON.stringify(user)}</> : <>{undefined}</>}</div>;
 }
 
 export default App;
