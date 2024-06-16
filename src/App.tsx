2732a6dd163e6fcc53ac827a7596ea241ae47346
b1on1kkk <b1on1kkk@mail.ru>
2024-06-16 14:44:05 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import { useEffect, useMemo, useState } from "react";
 
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
-async function getUser(user: WebAppUser) {
-  if (!user) return "do not have anything";
+interface User {
+  image: string;
+  name: string;
+  teamId: string | null;
+  userLevel: number;
+  language: string;
+  vibration: boolean;
+  coinAnimation: boolean;
+  referralsCount: number;
+}
 
+interface DataResponse {
+  message: string;
+  user: User;
+}
+
+async function getUser(user: WebAppUser): Promise<DataResponse | AxiosError> {
   try {
-    const data = await axios.post(
+    const data = await axios.post<DataResponse>(
       "http://localhost:3000/users/find_or_create_user",
       {
         _id: user.id,
@@ -16,32 +30,65 @@ async function getUser(user: WebAppUser) {
 
     return data.data;
   } catch (error) {
-    return error;
+    return error as AxiosError;
   }
 }
 
 function App() {
-  const [user, setUser] = useState<any>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [error, setError] = useState<AxiosError | null>(null);
 
   const tg = useMemo(() => {
     return window.Telegram.WebApp;
   }, []);
 
   useEffect(() => {
+    if (tg) tg.ready();
+
     if (tg && tg.initDataUnsafe.user) {
-      getUser(tg.initDataUnsafe.user).then((data) => {
-        setUser(data);
-      });
+      getUser(tg.initDataUnsafe.user).then(
+        (data: AxiosError | DataResponse) => {
+          if (data instanceof AxiosError) setError(data);
+          else setUser(data.user);
+        }
+      );
     }
+
+    return () => {
+      tg.close();
+    };
   }, [tg]);
 
   return (
-    <div>
-      <div>{JSON.stringify(user)}</div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignContent: "center"
+      }}
+    >
+      {user ? (
+        <>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center"
+            }}
+          >
+            <img
+              style={{ borderRadius: "9999px" }}
+              src={user.image}
+              alt="user image"
+            />
+          </div>
 
-      <div>------------------------------------</div>
+          <div>{JSON.stringify(user, null, 2)}</div>
+        </>
+      ) : (
+        <>Loading...</>
+      )}
 
-      <div>{JSON.stringify(tg.initDataUnsafe.user)}</div>
+      <div>{JSON.stringify(error)}</div>
     </div>
   );
 }
