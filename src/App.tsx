0fd1e933bd1805ba949a6caeb1285d73a9916c80
b1on1kkk <b1on1kkk@mail.ru>
2024-06-23 00:38:33 +0300

    update ngrok link

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ const useFindOrLoggedInUser = <T extends DataResponse>(
       if (!user) throw new Error("User is undefined");
 
       const response = await axios.post<T>(
-        "https://50c9-37-214-42-169.ngrok-free.app/users/find_or_create_user",
+        "https://ef54-37-214-42-169.ngrok-free.app/users/find_or_create_user",
         {
           _id: user.id,
           name: user.first_name
